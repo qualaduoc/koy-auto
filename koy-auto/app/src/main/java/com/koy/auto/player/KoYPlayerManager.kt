0f178a961e8646072af270d5dbdf91d251fd04c5
taoladuoc <nguyenthanhduocathy@gmail.com>
2026-09-18 09:07:25 +0700

    Khai báo opt-in Media3 đúng phạm vi

--- koy-auto/app/src/main/java/com/koy/auto/player/KoYPlayerManager.kt
+++ koy-auto/app/src/main/java/com/koy/auto/player/KoYPlayerManager.kt
@@ -20,6 +20,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
+@androidx.annotation.OptIn(markerClass = [androidx.media3.common.util.UnstableApi::class])
 object KoYPlayerManager {
 
     private var exoPlayer: ExoPlayer? = null
--- koy-auto/app/src/main/java/com/koy/auto/presentation/CarPresentation.kt
+++ koy-auto/app/src/main/java/com/koy/auto/presentation/CarPresentation.kt
@@ -17,7 +17,7 @@ import com.koy.auto.webview.KoYWebChromeClient
 import com.koy.auto.webview.KoYWebView
 import com.koy.auto.webview.KoYWebViewClient
 
-@androidx.media3.common.util.UnstableApi
+@androidx.annotation.OptIn(markerClass = [androidx.media3.common.util.UnstableApi::class])
 class CarPresentation(
     outerContext: Context,
     display: Display
